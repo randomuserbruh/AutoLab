cf51fe8eb9a73223ea2fbbcf39a25422783143b4
zceechu <95242484+zceechu@users.noreply.github.com>
2022-10-28 17:43:23 +0100

    Update ScreenshotOsc.m
    messing with path

--- ScreenshotOsc.m
+++ ScreenshotOsc.m
@@ -6,8 +6,11 @@ function ScreenshotOsc(TestName, oscdir, dest)
     mkdir(TestName);
     cd(TestName);
     testhome = cd;
+    oldpath = path;         %sus
+    path(oldpath, 'oscdir');    %sus
     cd(oscdir);
     cd 'Live Data';       % Hi
     copyfile('SCREENSHOT.png', testhome);
     cd(home);
+    path = oldpath;
 end
